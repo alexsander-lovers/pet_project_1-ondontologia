f5c327d438e319387c62780ba66ab442d8bcb5d9
alexsander-lovers <150514172+alexsander-lovers@users.noreply.github.com>
2026-09-04 00:24:41 +0300

    Дорабатывает шапку

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,8 @@
 
+const burgerButton = document.querySelector('.burger-menu');
+const headerMobileContiner = document.querySelector('.header__mobile-continer')
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('burger-menu--is-active');
+  headerMobileContiner.classList.toggle('header__mobile-continer--is-active')
+});
