33a9e88587008e0704d53c1e4f3b301635f187e6
alexsander-lovers <150514172+alexsander-lovers@users.noreply.github.com>
2026-09-03 17:49:05 +0300

    Создает проект + размечает и частично стилизует хедер

---
+++ gulp/tasks/html.js
@@ -0,0 +1,38 @@
+import fileInclude from 'gulp-file-include';
+import versionNumber from 'gulp-version-number';
+import htmlMin from 'gulp-htmlmin';
+import rename from 'gulp-rename';
+
+export const html = () => {
+  return app.gulp.src(app.path.source.html)
+  .pipe(app.plugins.plumber(
+    app.plugins.notify.onError({
+      title: 'HTML',
+      message: 'Error: <%= error.message %>'
+    })
+  ))
+  .pipe(fileInclude())
+  .pipe(app.plugins.if(app.isBuild, versionNumber({
+      'value': '%DT%',
+      'append': {
+        'key': '_v',
+        'cover': 0,
+        'to': [
+          'css', 'js'
+        ]
+      },
+      'output': {
+        'file': 'gulp/version.json'
+      }
+  })))
+  .pipe(app.gulp.dest(app.path.build.html))
+  .pipe(app.plugins.if(app.isBuild, htmlMin({
+    collapseWhitespace: true,
+    removeComments: true
+  })))
+  .pipe(app.plugins.if(app.isBuild, rename({
+    extname: '.min.html'
+  })))
+  .pipe(app.plugins.if(app.isBuild, app.gulp.dest(app.path.build.html)))
+  .pipe(app.plugins.browsersync.stream())
+};
